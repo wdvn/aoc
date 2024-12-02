4b7f5a9a5d1cb887965ef60cec1c132e8930478d
luynt <luynt@fpt.com.vn>
2024-12-02 14:12:55 +0700

    update day2

---
+++ day1.go
@@ -0,0 +1,52 @@
+package main
+
+//
+//import (
+//	"fmt"
+//	"os"
+//	"regexp"
+//	"sort"
+//	"strconv"
+//	"strings"
+//)
+//
+//func parse() ([]int, []int) {
+//	raw, _ := os.ReadFile("./input/day1.txt")
+//
+//	var l, r []int
+//	reg := regexp.MustCompile(`(\d+)\s+(\d+)`)
+//	for _, line := range strings.Split(string(raw), "\n") {
+//		tmp := reg.FindStringSubmatch(line)
+//		if len(tmp) < 3 {
+//			continue
+//		}
+//		n1, _ := strconv.Atoi(tmp[1])
+//		l = append(l, n1)
+//		n2, _ := strconv.Atoi(tmp[2])
+//		r = append(r, n2)
+//	}
+//	sort.Ints(l)
+//	sort.Ints(r)
+//	return l, r
+//}
+//
+//func main() {
+//	l, r := parse()
+//	var p1, p2 int
+//
+//	abs := func(a int) int {
+//		if a < 0 {
+//			return -a
+//		}
+//		return a
+//	}
+//	m1 := make(map[int]int, len(r))
+//	for i := range len(r) {
+//		m1[r[i]]++
+//	}
+//	for i := range len(l) {
+//		p1 += abs(l[i] - r[i])
+//		p2 += l[i] * m1[l[i]]
+//	}
+//	fmt.Println(p1, p2)
+//}
---
+++ day2.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	abs := func(a int) int {
+		if a <= 0 {
+			return -a
+		}
+		return a
+	}
+
+	conv := func(arr []string) []int {
+		t := make([]int, 0, len(arr))
+		for _, n := range arr {
+			v, _ := strconv.Atoi(n)
+			t = append(t, v)
+		}
+		return t
+	}
+
+	check := func(arr []int) bool {
+		base := arr[0] - arr[1]
+		for i := 0; i < len(arr)-1; i++ {
+			space := arr[i] - arr[i+1]
+			if abs(space) > 3 || abs(space) == 0 || base*space <= 0 {
+				return false
+			}
+		}
+		return true
+	}
+
+	raw, _ := os.ReadFile("./input/day2.txt")
+	var out int
+	for _, line := range strings.Split(string(raw), "\n") {
+		nums := strings.Split(line, " ")
+		if len(nums) < 2 {
+			out++
+			continue
+		}
+		numbers := conv(nums)
+		for i := range len(numbers) {
+			tmp := make([]int, len(numbers))
+			copy(tmp, numbers)
+			if check(slices.Delete(tmp, i, i+1)) {
+				out++
+				break
+			}
+		}
+
+	}
+	fmt.Println(out)
+}
