49c0fd694c00cae36a0f0329580f5e76fa0a6875
luynt <luynt@fpt.com.vn>
2024-12-03 15:40:31 +0700

    update

--- day3.go
+++ day3.go
@@ -7,45 +7,46 @@ import (
 	"strconv"
 )
 
+func cal(sub [][]byte) int64 {
+	n1, _ := strconv.Atoi(string(sub[1]))
+	n2, _ := strconv.Atoi(string(sub[2]))
+	return int64(n1 * n2)
+}
+
+func part1(s []byte) int64 {
+	reg := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+
+	subs := reg.FindAllSubmatch(s, -1)
+	var out int64
+	for _, sub := range subs {
+		out += cal(sub)
+	}
+	return out
+}
+
+func part2(s []byte) int64 {
+	enable := true
+	reg := regexp.MustCompile(`don't\(\)|do\(\)|mul\((\d+),(\d+)\)`)
+	var out int64
+	subs := reg.FindAllSubmatch(s, -1)
+	for _, sub := range subs {
+		switch sub[0][3] {
+		case ')':
+			enable = true
+		case "'"[0]:
+			enable = false
+		default:
+			if !enable {
+				continue
+			}
+			out += cal(sub)
+		}
+	}
+	return out
+}
 func main() {
 	raw, _ := os.ReadFile("./input/day3.txt")
 
-	part1 := func() {
-		reg := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
-
-		cal := reg.FindAllSubmatch(raw, -1)
-		var out int64
-		for _, sub := range cal {
-			n1, _ := strconv.Atoi(string(sub[1]))
-			n2, _ := strconv.Atoi(string(sub[2]))
-			out += int64(n1 * n2)
-		}
-		fmt.Println(out)
-	}
-	part1()
-
-	part2 := func() {
-		enable := true
-		reg := regexp.MustCompile(`don't\(\)|do\(\)|mul\((\d+),(\d+)\)`)
-		var out int64
-		cal := reg.FindAllSubmatch(raw, -1)
-		for _, sub := range cal {
-			switch string(sub[0]) {
-			case "do()":
-				enable = true
-			case "don't()":
-				enable = false
-			default:
-				if !enable {
-					continue
-				}
-				n1, _ := strconv.Atoi(string(sub[1]))
-				n2, _ := strconv.Atoi(string(sub[2]))
-				out += int64(n1 * n2)
-			}
-		}
-		fmt.Println(out, enable)
-	}
-
-	part2()
+	fmt.Println(part1(raw))
+	fmt.Println(part2(raw))
 }
