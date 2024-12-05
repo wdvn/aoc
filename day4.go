424c92d28037efe3221c3dcb6f90d020ece94262
luynt <luynt@fpt.com.vn>
2024-12-05 14:10:05 +0700

    day5

--- day4.go
+++ day4.go
@@ -1,90 +1,91 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"strings"
-)
-
-// XMAS
-func countXmas(grid [][]byte, r, c int) int {
-	var out int
-	var tmp []byte
-	directs := [][]int{{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}}
-	for _, direct := range directs {
-		var x, y int = r, c
-		for len(tmp) < 4 {
-			tmp = append(tmp, grid[x][y])
-			x += direct[0]
-			y += direct[1]
-			if x >= len(grid) || x < 0 || y >= len(grid[0]) || y < 0 {
-				break
-			}
-		}
-		if string(tmp) == "XMAS" {
-			out++
-		}
-		tmp = nil
-	}
-	return out
-}
-
-func countMas(grid [][]byte, r, c int) int {
-	var out int
-	var tmp []byte
-	directs := [][]int{{-1, -1}, {-1, 1}, {1, 1}, {1, -1}}
-
-	for _, direct := range directs {
-		var x, y int = r, c
-		x += direct[0]
-		y += direct[1]
-		if x >= len(grid) || x < 0 || y >= len(grid[0]) || y < 0 {
-			break
-		}
-		tmp = append(tmp, grid[x][y])
-	}
-	/*
-		M-S   M-M  S-M  S-S
-		M-S   S-S  S-M  M-M
-	*/
-	switch string(tmp) {
-	case "MSSM", "MMSS", "SMMS", "SSMM":
-		out++
-	}
-
-	return out
-}
-func part1(grid [][]byte) int {
-	var out int
-	for i, row := range grid {
-		for j, c := range row {
-			if c == 'X' {
-				out += countXmas(grid, i, j)
-			}
-		}
-	}
-	return out
-}
-
-func part2(grid [][]byte) int {
-	var out int
-	for i, row := range grid {
-		for j, c := range row {
-			if c == 'A' {
-				out += countMas(grid, i, j)
-			}
-		}
-	}
-	return out
-}
-
-func main() {
-	raw, _ := os.ReadFile("./input/day4.txt")
-	var grid [][]byte
-	for _, line := range strings.Split(string(raw), "\n") {
-		grid = append(grid, []byte(line))
-	}
-
-	fmt.Println(part1(grid))
-	fmt.Println(part2(grid))
-}
+//
+//import (
+//	"fmt"
+//	"os"
+//	"strings"
+//)
+//
+//// XMAS
+//func countXmas(grid [][]byte, r, c int) int {
+//	var out int
+//	var tmp []byte
+//	directs := [][]int{{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}}
+//	for _, direct := range directs {
+//		var x, y int = r, c
+//		for len(tmp) < 4 {
+//			tmp = append(tmp, grid[x][y])
+//			x += direct[0]
+//			y += direct[1]
+//			if x >= len(grid) || x < 0 || y >= len(grid[0]) || y < 0 {
+//				break
+//			}
+//		}
+//		if string(tmp) == "XMAS" {
+//			out++
+//		}
+//		tmp = nil
+//	}
+//	return out
+//}
+//
+//func countMas(grid [][]byte, r, c int) int {
+//	var out int
+//	var tmp []byte
+//	directs := [][]int{{-1, -1}, {-1, 1}, {1, 1}, {1, -1}}
+//
+//	for _, direct := range directs {
+//		var x, y int = r, c
+//		x += direct[0]
+//		y += direct[1]
+//		if x >= len(grid) || x < 0 || y >= len(grid[0]) || y < 0 {
+//			break
+//		}
+//		tmp = append(tmp, grid[x][y])
+//	}
+//	/*
+//		M-S   M-M  S-M  S-S
+//		M-S   S-S  S-M  M-M
+//	*/
+//	switch string(tmp) {
+//	case "MSSM", "MMSS", "SMMS", "SSMM":
+//		out++
+//	}
+//
+//	return out
+//}
+//func part1(grid [][]byte) int {
+//	var out int
+//	for i, row := range grid {
+//		for j, c := range row {
+//			if c == 'X' {
+//				out += countXmas(grid, i, j)
+//			}
+//		}
+//	}
+//	return out
+//}
+//
+//func part2(grid [][]byte) int {
+//	var out int
+//	for i, row := range grid {
+//		for j, c := range row {
+//			if c == 'A' {
+//				out += countMas(grid, i, j)
+//			}
+//		}
+//	}
+//	return out
+//}
+//
+//func main() {
+//	raw, _ := os.ReadFile("./input/day4.txt")
+//	var grid [][]byte
+//	for _, line := range strings.Split(string(raw), "\n") {
+//		grid = append(grid, []byte(line))
+//	}
+//
+//	fmt.Println(part1(grid))
+//	fmt.Println(part2(grid))
+//}
---
+++ day5.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"slices"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func isCorrect(order map[int][]int, arr []int) bool {
+	for i := range len(arr) {
+		for j := i + 1; j < len(arr); j++ {
+			if slices.Contains(order[arr[i]], arr[j]) {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func part1(order map[int][]int, update [][]int) int {
+	var out int
+	for _, up := range update {
+		tmp := slices.Clone(up)
+		if !isCorrect(order, tmp) {
+			continue
+		}
+
+		mid := len(tmp) / 2
+		out += tmp[mid]
+	}
+	return out
+}
+
+func part2(order map[int][]int, update [][]int) int {
+	var out int
+	for _, up := range update {
+		if !isCorrect(order, up) {
+			sort.Slice(up, func(i, j int) bool {
+				if slices.Contains(order[up[i]], up[j]) {
+					return false
+				}
+				return true
+			})
+			mid := len(up) / 2
+			out += up[mid]
+		}
+
+	}
+	return out
+}
+
+func main() {
+	raw, _ := os.ReadFile("./input/day5.txt")
+	order := map[int][]int{}
+	var update [][]int
+	for _, line := range strings.Split(string(raw), "\n") {
+		if strings.Contains(line, "|") {
+			n1, _ := strconv.Atoi(line[:2])
+			n2, _ := strconv.Atoi(line[3:])
+			order[n2] = append(order[n2], n1)
+		}
+		if strings.Contains(line, ",") {
+			var tmp []int
+			arr := strings.Split(line, ",")
+			for _, n := range arr {
+				v, _ := strconv.Atoi(n)
+				tmp = append(tmp, v)
+			}
+			update = append(update, tmp)
+		}
+	}
+	v1 := part1(order, update)
+	fmt.Println(v1)
+	v2 := part2(order, update)
+	fmt.Println(v2)
+}
