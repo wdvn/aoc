5e580d911f30e7ac8e4006f530864692039b2507
luynt <nguyenthanhluynd@gmail.com>
2024-12-16 22:38:48 +0700

    optimize travel time

--- day16.go
+++ day16.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"sort"
 )
 
@@ -44,16 +43,8 @@ func (d Day16) part1(raw []byte) int {
 	queue := Queue[cost]{}
 	visited := map[pairWithD]int{}
 	queue.Add(cost{x: start.x, y: start.y, cost: 0, direct: 1})
-	out := math.MaxInt
-	var chains int
-	//	mp := cloneMatrix(grid)
-	//	var chains []pair
-	//defer func() {
-	//	for _, n := range chains {
-	//		mp[n.x][n.y] = '*'
-	//	}
-	//	printMatrix(mp)
-	//}()
+	var out, chains int
+
 	for queue.Len() > 0 {
 		sort.Slice(queue, func(i, j int) bool {
 			return queue[i].cost < queue[j].cost
@@ -65,25 +56,19 @@ func (d Day16) part1(raw []byte) int {
 		}
 		visited[pd] = 1
 		for f, d := range FourDirections {
-
 			x, y := q.x+d[0], q.y+d[1]
 			if isEscape(grid, x, y) || grid[x][y] == '#' {
 				continue
 			}
 			if grid[x][y] == 'E' {
-				if out > q.cost+1 {
-					chains = q.chains + 1
-					out = q.cost + 1
-				}
+				chains = q.chains + 1
+				out = q.cost + 1
 				break
 			}
 			c := q.cost + 1
 			if f != q.direct {
 				c += 1000
 			}
-			//next := pair{x, y}
-			//blocks := slices.Clone(q.chains)
-			//queue.Add(cost{x: x, y: y, cost: c, direct: f, chains: append(blocks, next)})
 			queue.Add(cost{x: x, y: y, cost: c, direct: f, chains: q.chains + 1})
 		}
 	}
